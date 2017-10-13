ade621da29139e45e70f45bbeebf851d07a9e446
why520crazy <why520crazy@163.com>
2017-10-13 11:13:30 +0800

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,13 @@ module.exports = {
   entry: "./src/index",
 
   output: {
-    // webpack 如何输出结果的相关选项
-
-    path: path.resolve(__dirname, "dist"), // string
-    // 所有输出文件的目标路径
-    // 必须是绝对路径（使用 Node.js 的 path 模块）
-
-    filename: "semver.js", // string
-
-    library: "semverLite", // string,
-    libraryTarget: "umd", // 通用模块定义
+    path: path.resolve(__dirname, "dist"),
+    filename: "semver.js",
+    library: "semverLite",
+    libraryTarget: "umd"
   },
 
   module: {
-    // 关于模块配置
-
     rules: [
       {
         test: /\.js$/,
@@ -34,25 +26,12 @@ module.exports = {
   },
 
   resolve: {
-    // 解析模块请求的选项
-    // （不适用于对 loader 解析）
-
     modules: [
       "node_modules",
       path.resolve(__dirname, "src")
     ],
-    // 用于查找模块的目录
-
     extensions: [".js", ".json"],
   },
-
-
-  context: __dirname, // string（绝对路径！）
-  // webpack 的主目录
-  // entry 和 module.rules.loader 选项
-  // 相对于此目录解析
-
-  target: "web", // 枚举
-  // 包(bundle)应该运行的环境
-  // 更改 块加载行为(chunk loading behavior) 和 可用模块(available module)
+  context: __dirname,
+  target: "web"
 };
